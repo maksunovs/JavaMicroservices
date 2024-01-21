4ec5d8015868505c81bb58e376841662c906c900
maksunovs <maksunovs@mail.ru>
2024-01-21 18:26:53 +0300

    Test for ResourceProcessor

--- ResourceProcessor/src/test/java/com/java/cources/ResourceProcessorApplicationTests.java
+++ ResourceProcessor/src/test/java/com/java/cources/ResourceProcessorApplicationTests.java
@@ -1,13 +1,25 @@
 package com.java.cources;
 
+import com.java.cources.parser.AudioFileParser;
+import com.java.cources.reader.S3StorageReader;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ResourceProcessorApplicationTests {
 
+
+
+	@Autowired
+	private AudioFileParser parser;
+
 	@Test
 	void contextLoads() {
+		S3StorageReader reader = new S3StorageReader("http://localhost:9000", "minio_user", "minio_password");
+		String metadata = parser.mapToSongJson(parser.parseMetadata(reader.readFile("test-bucket", "tjeFMCAo")));
+		System.out.println(metadata);
 	}
 
 }
