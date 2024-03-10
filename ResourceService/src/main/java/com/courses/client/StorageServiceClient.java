473efbc1260fdfdbeaa0a63b170228e2ef38ad8b
maksunovs <maksunovs@mail.ru>
2024-03-10 13:47:04 +0300

    Circuit breaker impl

--- ResourceService/src/main/java/com/courses/client/StorageServiceClient.java
+++ ResourceService/src/main/java/com/courses/client/StorageServiceClient.java
@@ -3,6 +3,8 @@ package com.courses.client;
 import com.courses.client.dto.StorageDto;
 import com.courses.client.dto.StorageListDto;
 import com.squareup.okhttp.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,27 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
+@Slf4j
 public class StorageServiceClient {
     @Value("${api.gateway.url}")
     private String API_GATEWAY_URL;
 
+    private StorageListDto storageListDtoStub;
+
+    @CircuitBreaker(name = "getStorages", fallbackMethod = "fallbackForGetStorages")
     public StorageListDto getStorages() {
+        log.info("Reading storages...");
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(API_GATEWAY_URL + "/storages", StorageListDto.class);
+        StorageListDto storageListDto = restTemplate.getForObject(API_GATEWAY_URL + "/storages", StorageListDto.class);
+        storageListDtoStub = storageListDto;
+        return storageListDto;
+    }
+
+    public StorageListDto fallbackForGetStorages(Exception e) {
+        log.error("Executing fallbackForGetStorages(): {}", e.getMessage());
+        return Optional.ofNullable(storageListDtoStub).orElseThrow(()->new RuntimeException("Not stub was created for storages."));
     }
 }
