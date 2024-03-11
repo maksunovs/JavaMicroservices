0f860e44c6b92f07ee0bf84a633113362c22fe31
maksunovs <maksunovs@mail.ru>
2024-03-11 10:48:04 +0300

    Auth Server 2 impl

--- AuthServer/src/main/java/com/courses/java/config/AuthServerConfig.java
+++ AuthServer/src/main/java/com/courses/java/config/AuthServerConfig.java
@@ -1,8 +1,6 @@
 package com.courses.java.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
