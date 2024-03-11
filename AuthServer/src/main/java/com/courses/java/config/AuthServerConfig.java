615c6b69302faa4d41bf92d9e877c1ff69fa629a
maksunovs <maksunovs@mail.ru>
2024-03-11 13:15:52 +0300

    Auth Server 2 impl

--- AuthServer/src/main/java/com/courses/java/config/AuthServerConfig.java
+++ AuthServer/src/main/java/com/courses/java/config/AuthServerConfig.java
@@ -1,25 +1,25 @@
-package com.courses.java.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-public class AuthServerConfig extends GlobalAuthenticationConfigurerAdapter {
-    @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("filip")
-                .password(passwordEncoder().encode("1234"))
-                .roles("ADMIN");
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-}
+//package com.courses.java.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+//public class AuthServerConfig extends GlobalAuthenticationConfigurerAdapter {
+//    @Override
+//    public void init(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("filip")
+//                .password(passwordEncoder().encode("1234"))
+//                .roles("ADMIN");
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//}
--- ResourceService/src/main/java/com/courses/client/StorageServiceClient.java
+++ ResourceService/src/main/java/com/courses/client/StorageServiceClient.java
@@ -2,11 +2,22 @@ package com.courses.client;
 
 import com.courses.client.dto.StorageDto;
 import com.courses.client.dto.StorageListDto;
+import com.netflix.discovery.converters.Auto;
 import com.squareup.okhttp.*;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,12 +32,30 @@ public class StorageServiceClient {
     @Value("${api.gateway.url}")
     private String API_GATEWAY_URL;
 
+    @Autowired
+    private OAuth2AuthorizedClientService clientService;
     private StorageListDto storageListDtoStub;
 
     @CircuitBreaker(name = "getStorages", fallbackMethod = "fallbackForGetStorages")
     public StorageListDto getStorages() {
+
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        OAuth2AuthenticationToken oauthToken =
+                (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(
+                oauthToken.getAuthorizedClientRegistrationId(),
+                oauthToken.getName());
+
+        String accessToken = client.getAccessToken().getTokenValue();
+
+
         log.info("Reading storages...");
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(getBearerTokenInterceptor(accessToken));
         StorageListDto storageListDto = restTemplate.getForObject(API_GATEWAY_URL + "/storages", StorageListDto.class);
         storageListDtoStub = storageListDto;
         return storageListDto;
@@ -34,6 +63,20 @@ public class StorageServiceClient {
 
     public StorageListDto fallbackForGetStorages(Exception e) {
         log.error("Executing fallbackForGetStorages(): {}", e.getMessage());
-        return Optional.ofNullable(storageListDtoStub).orElseThrow(()->new RuntimeException("Not stub was created for storages."));
+        return Optional.ofNullable(storageListDtoStub).orElseThrow(() -> new RuntimeException("Not stub was created for storages."));
+    }
+
+    private ClientHttpRequestInterceptor
+    getBearerTokenInterceptor(String accessToken) {
+        ClientHttpRequestInterceptor interceptor =
+                new ClientHttpRequestInterceptor() {
+                    @Override
+                    public ClientHttpResponse intercept(HttpRequest request, byte[] bytes,
+                                                        ClientHttpRequestExecution execution) throws IOException {
+                        request.getHeaders().add("Authorization", "Bearer " + accessToken);
+                        return execution.execute(request, bytes);
+                    }
+                };
+        return interceptor;
     }
 }
